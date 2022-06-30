44fa474484c196ac5307390c0edb5be66a765bc5
Juancaceres963 <105230103+Juancaceres963@users.noreply.github.com>
2022-06-30 17:29:10 -0300

    Complete

--- JSProjects/MagicEightBall.js
+++ JSProjects/MagicEightBall.js
@@ -1,1 +1,47 @@
+let userName = '';
+switch (userName) {
+  case userName :
+    console.log(`Hello ${userName}!`);
+  break;
+  default:
+    console.log(`Hello!`);
+  break;
+  }
 
+  let userQuestion = '';
+  console.log(`Estas seguro ${userName} de que quieres saber la respuesta a esta pregunta.. ${userQuestion}. `)
+
+  let randomNumber = Math.floor(Math.random() * 10);
+
+  let eightBall = '';
+
+  switch (randomNumber) {
+    case 0 : 
+     eightBall = ('It is certain');
+    break;
+    case 1 : 
+     eightBall = ('It is decidedly so');
+    break;
+    case 2 : 
+     eightBall = ('Reply hazy try again');
+    break;
+    case 3 : 
+     eightBall = ('Cannot predict now');
+    break;
+    case 4 : 
+     eightBall = ('Do not count on it');
+    break;
+    case 5 : 
+     eightBall = ('My sources say no');
+    break;
+    case 6 : 
+     eightBall = ('Outlook not so good');
+    break;
+    case 7 : 
+     eightBall = ('Signs point to yes');
+    break;
+    default:
+     eightBall = ('Todo va a estar bien')
+  }
+
+  console.log(eightBall);
